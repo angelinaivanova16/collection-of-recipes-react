ae9b36b20f49e94a1ed4edabd39e88e453056895
Angelina <angelinaivanova16@gmail.com>
2024-04-28 02:23:44 +0300

    refactor: configure eslint, prettier

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import { Outlet, Link } from 'react-router-dom';
 
-function App() {
+function App() {  
   return (
     <div className="App">
       Yo
