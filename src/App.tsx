67ba1fa27d7a6c890e54d1a8b45c0281a71ec239
Angelina <angelinaivanova16@gmail.com>
2024-05-02 01:25:48 +0300

    fix: fix deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
-// import { Outlet, Link } from 'react-router-dom';
 
 function App() {  
   return (
-    <Routes>
-      <Route path='' element={}/>
-    </Routes>
+    <div className="App">
+      Hello!
+    </div>
   );
 }
 
