e0e29083e41a264497284c49ffa01c126db78887
Angelina <angelinaivanova16@gmail.com>
2024-05-12 23:32:10 +0300

    Updates

---
+++ static/js/819.d54567e2.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcollection_of_recipes_react=self.webpackChunkcollection_of_recipes_react||[]).push([[819],{819:(e,s,a)=>{a.r(s),a.d(s,{default:()=>d});const c="cardsList_cardsList__go0qn";var i=a(5808),n=a(7468),t=a(2043),r=a(579);const o=()=>{const{data:e,isLoading:s}=(0,n.Dh)(),a=e;return s?(0,r.jsx)(t.Z,{}):(0,r.jsx)("main",{className:c,children:a.map((e=>(0,r.jsx)(i.Z,{id:e.id,name:e.name,image:e.image},e.id)))})},d=()=>(0,r.jsx)(o,{})}}]);
+//# sourceMappingURL=819.d54567e2.chunk.js.map
